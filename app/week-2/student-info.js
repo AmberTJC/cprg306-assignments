6078a50db3ccefc58262f7f8c50f6a90341ebaa0
AmberTJC <147658220+AmberTJC@users.noreply.github.com>
2024-09-13 15:12:26 -0600

    Added headings, created links, created component
    Created the headings for both the main and week 2 pages and created the links between the main and week 2 pages. Also created the Student info component.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+return(
+    <main>
+    <p>Amber Campbell</p>
+    <Link href = "https://github.com/AmberTJC/cprg306-assignments">Github Account</Link>
+    </main>
+
+
+);
+}
