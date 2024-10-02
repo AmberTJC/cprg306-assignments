484682972321f3196dae2fd8b84ca65d0f291bc5
AmberTJC <147658220+AmberTJC@users.noreply.github.com>
2024-10-01 19:59:35 -0600

    Couldn't figure out how to display list
    List wouldn't display. Couldn't get the item list to connect to the item component.

---
+++ app/week-3/page.js
@@ -0,0 +1,14 @@
+
+import ItemList from "./item-list";
+
+
+
+export default function Page(){
+
+    return(
+<main>
+    <h3 className="font-bold text-teal-800">Shopping List</h3>
+    
+</main>
+    );
+}
